8054612fe5c8585fff9da0d95f5dac7c5e4e4524
aperdomoll90 <aperdomoll90@gmail.com>
2021-04-16 15:24:03 -0400

    API 80% done

--- index.js
+++ index.js
@@ -11,8 +11,18 @@ const db = admin.firestore()
 
 db.collection('products').get()
 .then(collection => {
-    collection.forEach(doc => {
-        console.log(doc.data())
+    let productArray = []
+   collection.forEach(doc => {
+        const thisData = doc.data()
+        const thisProduct = {
+            id: doc.id,
+            name: thisData.name || thisData.Name,
+            category: thisData.category || thisData.Category,
+            vendor: thisData.vendor || thisData.Vendor,
+        }
+        thisProduct.id = doc.id
+        productArray.push(thisProduct)
     })
+    console.log(productArray)
 })
 .catch(err => console.log('ERROR GETTING PRODUCT:', err))

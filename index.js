a634dc2ea4e6dceb2618c09d0bfdcfec6992cdb8
aperdomoll90 <aperdomoll90@gmail.com>
2021-04-16 15:01:00 -0400

    continued ORM practice

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 const admin =require('firebase-admin')
 
-const credentials = require('./credentials.jason')
+const credentials = require('./credentials.json')
 admin.initializeApp({
     credential:admin.credential.cert(credentials)
 })
 
 const db = admin.firestore()
+
+// now we can run CRUD
+
+db.collection('products').get()
+.then(collection => {
+    collection.forEach(doc => {
+        console.log(doc.data())
+    })
+})
+.catch(err => console.log('ERROR GETTING PRODUCT:', err))
